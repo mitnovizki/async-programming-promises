3dc19ae0e3b4ab287980aeb8c11dd9609cac2f87
Queen King <king@Queens-iMac.local>
2021-02-28 18:33:21 +0200

    arrow | iife | set

--- src/functions.mjs
+++ src/functions.mjs
@@ -49,22 +49,81 @@ export function declaration_expression() {
     appendText('function expression')
   }
 }
-
+/* EXAMPLE 1
 export function closures() {
   function name(name) {
+    setText(' ');
     return function lastName(lastName) {
-      appendText(`${name},  ${lastName}`)
+      appendText(`${name}  ${lastName}`)
     }
   }
   const myName = name('Aimee ');
   myName(' Mitnovitsky');
   myName(' Meatnwhisky');
 }
+*/
+export function closures() {
+  setText('use closures to generate different domains\n')
+  function buildUrl(domain) {
+    return function url(name) {
+      appendText(`http://www.${name}.${domain}  `)
+    }
+  }
+  let domainCom = buildUrl('com')
+  domainCom('google')
+  domainCom('yahoo')
+  domainCom('gmail')
+  domainCom('facebook')
 
-export function chain() {
+  let domainIl = buildUrl('co.il')
+  domainIl('kneset')
+  domainIl('ynet')
+
+  let domainRu = new buildUrl('ru')
+  domainRu('putinclub')
+  domainRu('moscow')
+  domainRu('rusianfederationyeah')
 }
 
-export function chainCatch() {
+export function set() {
+
+  setText(' use set to remove duplocated values')
+  let nums = [1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8, 9, 9, 9]
+  appendText(nums)
+  var set = new Set(nums);
+  appendText('  ')
+  appendText(([...set]))
+}
+
+export function arrow() {
+  setText('=>')
+  function greating(name) {
+    setText(`hi, ${name}`)
+  }
+
+  const arrowFunction = yourName => setText(`hi, ${yourName}`)
+  arrowFunction('Amme');
+
+  const arrowFunction2 = num => num ** num
+  alert(`√3: ${arrowFunction2(3)}`
+  )
+
+}
+
+export function iife() {
+  setText('IIFE')
+    (function greating(name) {
+      appendText(`\n hi, ${name}`)
+    }('amme'))
+}
+export function map() {
+
+}
+export function reduce() {
+
+}
+export function thisThat() {
+
 }
 
 export function final() {
